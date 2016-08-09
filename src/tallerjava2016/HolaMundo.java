0072da72223c8fe08f6809313c3989f46cc48cc7
Israel <Israel@10.20.0.125>
2016-08-09 17:24:15 -0500

    Segundo Commit

---
+++ src/tallerjava2016/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerjava2016;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author Israel
+ */
+public class HolaMundo {
+
+    @Override
+    public String toString() {
+        return "HolaMundo{" + '}';
+    }
+    
+}
